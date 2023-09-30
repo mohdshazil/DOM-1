f750d3946aab160609a2d484e47c3c01648cfb97
mohd shazil <mohdshazil18871@gmail.com>
2023-10-01 00:24:22 +0530

    first file dom is ready

---
+++ script.js
@@ -0,0 +1,36 @@
+// let div1 = document.getElementById("div1");
+// console.log("div1");
+
+// div1.addEventListener("mouseover", function(eventOver){
+//     console.log(eventOver.timeStamp);
+//     console.log("mouse over event is activate");
+// });
+
+
+
+// let datas={
+//     button1 : "data1",
+//     button2 : "data2",
+//     button3 : "data3"
+// }
+
+// let btn1 = document.getElementById("bt1");
+
+// btn1.addEventListener("click", function(){
+//     let data = document.getElementById("data");
+//     data.innerText=datas["button1"]
+// });
+
+
+
+let div1 = document.getElementById("div1");
+console.log(div1);
+
+div1.style.backgroundColor = "yellow";
+div1.style.width = "300px";
+div1.style.height = "200px";
+div1.style.margin = "20px";
+div1.style.color = "blue";
+div1.style.padding = "10px";
+div1.style.fontSize = "18px";
+div1.style.fontWeight = "bold";
